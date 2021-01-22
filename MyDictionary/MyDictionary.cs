55d004b103f098f217c759d2e9b86ca1f64b9639
Melike Öztekin <77509002+melikeoztekin@users.noreply.github.com>
2021-01-23 00:56:46 +0300

    20 Ocak dersinin ödevi eklendi (odev5)

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2,T3,T4>
+    {
+        T1[] _numbers;
+        T2[] _names;
+        T3[] _sizes;
+        T4[] _weights;
+        
+        public MyDictionary()
+        {
+            _numbers = new T1[0];
+            _names = new T2[0];
+            _sizes = new T3[0];
+            _weights = new T4[0];
+        }
+        public void Add(T1 _number,T2 _name,T3 _size,T4 _weight)
+        {
+            T1[] _tempNumbers = _numbers;
+            T2[] _tempNames = _names;
+            T3[] _tempSizes = _sizes;
+            T4[] _tempWeights = _weights;
+
+            _numbers = new T1[_numbers.Length + 1];
+            _names = new T2[_names.Length + 1];
+            _sizes = new T3[_sizes.Length + 1];
+            _weights = new T4[_weights.Length + 1];
+
+            for (int i = 0; i < _tempNumbers.Length; i++)
+            {
+                _numbers[i] = _tempNumbers[i];
+                _names[i] = _tempNames[i];
+                _sizes[i] = _tempSizes[i];
+                _weights[i] = _tempWeights[i];
+            }
+
+            _numbers[_numbers.Length - 1] = _number;
+            _names[_names.Length - 1] = _name;
+            _sizes[_sizes.Length - 1] = _size;
+            _weights[_weights.Length - 1] = _weight;
+        }
+        public int Length
+        {
+            get { return _numbers.Length; }
+        }
+        public T1[] Tnumber
+        {
+            get { return _numbers; }
+        }
+
+        public T2[] Tname
+        {
+            get { return _names; }
+        }
+
+        public T3[] Tsize
+        {
+            get { return _sizes; }
+        }
+        public T4[] Tweight
+        {
+            get { return _weights; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string, double, int> identity = new MyDictionary<int, string, double, int>();
+            identity.Add(123, "Melike Öztekin", 1.63, 46);
+            identity.Add(234, "Kadriye Çakmak", 1.58, 50);
+            identity.Add(345, "Ceylan Çokadar", 1.53, 48);
+            identity.Add(456, "Umut Kayra", 1.23, 27);
+
+            Console.WriteLine("Identity length = " + identity.Length);
+
+            Console.WriteLine("--------Identity Numbers--------");
+            foreach (var number in identity.Tnumber)
+            {
+                Console.WriteLine(+number);
+            }
+
+            Console.WriteLine("--------Identity Names----------");
+            foreach (var name in identity.Tname)
+            {
+                Console.WriteLine(name);
+            }
+            
+            Console.WriteLine("--------Identity Sizes----------");
+            foreach (var size in identity.Tsize)
+            {
+                Console.WriteLine(size);
+            }
+            
+            Console.WriteLine("--------Identity Weights----------");
+            foreach (var weight in identity.Tweight)
+            {
+                Console.WriteLine(weight);
+            }
+        }
+    }
+}
